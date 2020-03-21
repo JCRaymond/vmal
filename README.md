839eac825ad6c7f860de28821281bb2971b2837e
Jacob <raymond.jacob17@gmail.com>
2020-03-21 00:11:58 -0400

    Refactored VMAL.py to be better organized, updated README

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 This repository holds the reference implementation for VMAL, an assembly language for the Vertical Microarchitecture discussed in ITSC 3181 at UNC-Charlotte.
 
 ## Requirements
-All of the code for VMAL is contained within VMAL.py, which requires Python 3.5 or greater (tested on 3.5 and later, it might work on earlier versions of python) as well as the package `easygui` for graphical file choosing, which can be installed with pip:
+All of the code for VMAL is contained within VMAL.py, which requires Python 3.6 or greater. Optionally, if you wish to use the file explorer to select the code to run, the `easygui` package is required, which can be installed with pip:
 ```bash
 pip install --user easygui
 ```
@@ -10,9 +10,9 @@ pip install --user easygui
 ## Running VMAL
 In order to run VMAL, there are two options. The first is to just run VMAL with python, i.e.
 ```bash
-python VMAL.py
+python3.6 VMAL.py
 ```
-Which will open a file browser to select the .vmal file to execute, or you may pass the file directly as a command line argument, which will supress the file browser prompt
+Which will open a file browser to select the .vmal file to execute (this requires `easygui`). Alternatively, you may pass the file directly as a command line argument, which will supress the file browser prompt (and does not require the `easygui` package.
 ```bash
-python VMAL.py my_code.vmal
+python3.6 VMAL.py my_code.vmal
 ```
--- VMAL.py
+++ VMAL.py
@@ -1,145 +1,182 @@
 import VMALAssembler
+from collections import defaultdict
 from pprint import pprint
 
-int32_max = 0xffffffff
-
-def printregisters(arr):
-    print(*(str(hex(i))[2:].upper() + ': ' + str(v if v & 0x80000000 == 0 else -((-v) & int32_max)) for i,v in enumerate(arr)), sep='\n')
-
 ops = ['SA', 'RB', 'RD', 'WR', 'SB', 'SF', 'GO', 'BIN', 'BIZ', 'ADD', 'AND', 'MV', 'NOT', 'RS', 'LS', 'SW']
-def printop(op):
+def op_str(op):
    op, *params = op
    if op == 6 or op == 7 or op == 8:
       params[0] += 1
       params = map(str, params)
    else:
       params = map(lambda p: str(hex(p))[2:].upper(), params)
-   print(ops[op], ', '.join(params))
+   return f'{ops[op]} {", ".join(params)}'
 
-def runcode(code, debug=False):
-   code, regstate, memstate = code
-   
-   state = [0] * 16
-   for reg, val in regstate:
-      state[reg] = val & int32_max
-   state[5] = 0
-   state[6] = 1
-   state[7] = int32_max
+def printcode(code):
+   print(*(f'{i: 3}: {op_str(op)}' for i, op in enumerate(code)), sep = '\n')
 
-   mem = {}
-   for loc, val in memstate:
-      mem[loc] = val
+def get_int(reg_val):
+   return (reg_val if reg_val & 0x80000000 == 0 else -((-reg_val) & VM.int_max))
 
-   MAR = 0
-   MBR = 0
-   N = False
-   Z = False
+class VM:
+   int_max = 0xffffffff
 
-   def SA(X):
-      nonlocal MAR
-      MAR = state[X]
-   def RB(X):
-      state[X] = MBR
-   def RD():
-      nonlocal MBR
-      MBR = 0
-      if MAR in mem:
-         MBR = mem[MAR]
-   def WR():
-      mem[MAR] = MBR
-   def SB(X):
-      nonlocal MBR
-      MBR = state[X]
-   def SF(X):
-      nonlocal Z, N
-      Z = (state[X] == 0)
-      N = bool(state[X] & 0x80000000)
-   def GO(I):
-      state[0] = I
-   def BIN(I):
-      if N:
-         state[0] = I
-   def BIZ(I):
-      if Z:
-         state[0] = I
-   def ADD(X,Y):
-      state[X] = (state[X] + state[Y]) & int32_max
-   def AND(X,Y):
-      state[X] = (state[X] & state[Y]) & int32_max
-   def MV(X,Y):
-      state[X] = state[Y]
-   def NOT(X,Y):
-      state[X] = (~state[Y]) & int32_max
-   def RS(X,Y):
-      state[X] = (state[Y] >> 1) & int32_max
-   def LS(X,Y):
-      state[X] = (state[Y] << 1) & int32_max
-   def SW(X,Y):
-      nonlocal MAR, MBR
-      MAR = state[X]
-      MBR = state[Y]
-      mem[MAR] = MBR
+   def __init__(self, reg_inits=[], mem_inits=[]):
+      self.registers = [0]*16
+      for reg, val in reg_inits:
+         self.registers[reg] = val & VM.int_max
+         
+      self.registers[0] = 0
+      self.registers[5] = 0
+      self.registers[6] = 1
+      self.registers[7] = VM.int_max
+
+      self.memory = defaultdict(lambda: 0)
+      for loc, val in mem_inits:
+         self.memory[loc] = val & VM.int_max
+
+      self.MAR = 0
+      self.MBR = 0
+      self.N = False
+      self.Z = False
+
+   def setreg(self, reg, val):
+      self.registers[reg] = val & VM.int_max
+
+   def setmem(self, loc, val):
+      self.memory[loc] = val
+
+   def printregisters(self):
+      print()
+      print('Registers:')
+      for i, v in enumerate(self.registers):
+         print(f'{str(hex(i))[2:].upper()}: {get_int(v)}')
+      print()
+
+   def printmemory(self):
+      print()
+      print('Memory:')
+      items = iter(sorted(self.memory.items()))
+      loc, v = next(items)
+      print(f'[{loc}]: {get_int(v)}')
+      last = loc
+      for loc, v in items:
+         if loc - last > 1:
+            print(f'... {loc - last - 1} empty locations ...')
+         print(f'[{loc}]: {get_int(v)}')
+         last = loc
+      print()
+
+   def SA(self, X):
+      self.MAR = self.registers[X]
+   def RB(self, X):
+      self.registers[X] = self.MBR
+   def RD(self):
+      self.MBR = 0
+      if self.MAR in self.memory:
+         self.MBR = self.memory[self.MAR]
+   def WR(self):
+      self.memory[self.MAR] = self.MBR
+   def SB(self, X):
+      self.MBR = self.registers[X]
+   def SF(self, X):
+      self.Z = (self.registers[X] == 0)
+      self.N = bool(self.registers[X] & 0x80000000)
+   def GO(self, I):
+      self.registers[0] = I
+   def BIN(self, I):
+      if self.N:
+         self.registers[0] = I
+   def BIZ(self, I):
+      if self.Z:
+         self.registers[0] = I
+   def ADD(self, X, Y):
+      self.registers[X] = (self.registers[X] + self.registers[Y]) & VM.int_max
+   def AND(self, X, Y):
+      self.registers[X] = (self.registers[X] & self.registers[Y]) & VM.int_max
+   def MV(self, X, Y):
+      self.registers[X] = self.registers[Y]
+   def NOT(self, X, Y):
+      self.registers[X] = (~self.registers[Y]) & VM.int_max
+   def RS(self, X, Y):
+      self.registers[X] = (self.registers[Y] >> 1) & VM.int_max
+   def LS(self, X, Y):
+      self.registers[X] = (self.registers[Y] << 1) & VM.int_max
+   def SW(self, X, Y):
+      self.MAR = self.registers[X]
+      self.MBR = self.registers[Y]
+      self.memory[self.MAR] = self.MBR
 
    ops = [SA, RB, RD, WR, SB, SF, GO, BIN, BIZ, ADD, AND, MV, NOT, RS, LS, SW]
 
-   def runop(op, *params):
-      ops[op](*params)
+   def runop(self, op, *params):
+      self.ops[op](self, *params)
 
-   state[0] = 0
-   bp = set()
-   quit = False
-   cont = False
-   while state[0] < len(code) and not quit:
-      op = code[state[0]]
-      on_bp = state[0] in bp
-      if debug and (not cont or on_bp):
-         print()
-         print('Registers:')
-         printregisters(state)
-         
-         print()
-         print('Flags:')
-         print('N:', N)
-         print('Z:', Z)
-         
-         print()
-         if cont:
-            print('Continue till Breakpoint')
-         if on_bp:
-            print('BREAKPOINT')
-         print('Operation: ',end='')
-         printop(op)
-         
-         while True:
-            resp = input('Debug (n,b,c,r,q): ')
-            if len(resp) == 0:
-               resp = 'n'
-            else:
-               resp = resp[0].lower()
-            if resp == 'n':
-               pass
-            elif resp == 'b':
-               print('Turning Breakpoint ', end='')
-               if state[0] in bp:
-                  bp.remove(state[0])
-                  print('OFF')
+   def runcode(self, code, limit = float('inf')):
+      i = 0
+      while self.registers[0] < len(code):
+         if i >= limit:
+            return False
+         op = code[self.registers[0]]
+         self.runop(*op)
+         self.registers[0] += 1
+         i += 1
+      return True
+
+   def rundebug(self, code, limit = float('inf')):
+      i = 0
+      bp = set()
+      cont = False
+      while self.registers[0] < len(code):
+         if i >= limit:
+            return False
+         op = code[self.registers[0]]
+         on_bp = self.registers[0] in bp
+         if not cont or on_bp:
+            self.printregisters()
+            
+            print('Flags:')
+            print('N:', self.N)
+            print('Z:', self.Z)
+            
+            print()
+            if cont:
+               print('Continue till Breakpoint')
+            if on_bp:
+               print('BREAKPOINT')
+            print(f'Operation: {op_str(op)}',end='')
+            
+            while True:
+               resp = input('Debug (n,b,c,r,q): ')
+               if len(resp) == 0:
+                  resp = 'n'
                else:
-                  bp.add(state[0])
-                  print('ON')
-               continue
-            elif resp == 'c':
-               cont = not cont
-            elif resp == 'r':
-               debug = False
-            elif resp == 'q':
-               return state, mem
-            else:
-               print('Invalid operation, please try again')
-               continue
-            break
-      runop(*op)
-      state[0] += 1
-   return state, mem
+                  resp = resp[0].lower()
+               if resp == 'n':
+                  pass
+               elif resp == 'b':
+                  print('Turning Breakpoint ', end='')
+                  if self.registers[0] in bp:
+                     bp.remove(self.registers[0])
+                     print('OFF')
+                  else:
+                     bp.add(self.registers[0])
+                     print('ON')
+                  continue
+               elif resp == 'c':
+                  cont = not cont
+               elif resp == 'r':
+                  debug = False
+               elif resp == 'q':
+                  return False
+               else:
+                  print('Invalid operation, please try again')
+                  continue
+               break
+         self.runop(*op)
+         self.registers[0] += 1
+         i += 1
+      return True
 
 def main():
    from sys import argv, executable
@@ -168,34 +205,38 @@ def main():
    else:
       fname = argv[1]
    
-   print('Compiling...')
+   print('Assembling...')
    with open(fname, 'r') as codefile:
       machine_code = VMALAssembler.assemble(codefile)
    if machine_code is None:
-      print('Failed to complie, terminating')
+      print('Failed to assemble, terminating')
       return
-   print('Compiled!')
+   print('Assembled!')
+
+   code, reg_inits, mem_inits = machine_code
+   vm = VM(reg_inits, mem_inits)
    
    resp = input('Press ENTER to run the program, or type "d" or "debug" to run the program in debug mode: ')
    
    if len(resp) == 0 or resp[0].lower() != 'd':
-      finalstate, mem = runcode(machine_code)
+      success = vm.runcode(code)
    else:
       print()
       print('Assembled Code:')
       for i, op in enumerate(machine_code[0]):
          print(i, ': ', sep='', end='')
          printop(op)
-      finalstate, mem = runcode(machine_code, debug = True)
+      success = vm.rundebug(machine_code)
+
+   if not success:
+      print('Code run was not successful (operation limit reached or program quit), terminating')
+      return
    
-   print()
-   print('Final Register State:')
-   printregisters(finalstate)
-   print()
-   for k in mem:
-      v = mem[k]
-      mem[k] = v if v & 0x80000000 == 0 else -((-v) & int32_max)
-   pprint(mem)
+   vm.printregisters()
+
+   if len(vm.memory) > 0:
+      vm.printmemory()
+   
    
 if __name__ == "__main__":
    try:
